87da4e1953c507794de0bbc95fee5d2a9055cff3
roger <rjpd@netzero.com>
2018-12-22 15:51:35 -0500

    init

---
+++ Poker/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    public class Card
+    {
+        public Card() : this(new Random()) { }
+
+        public Card(Random random)
+        {
+            Number = random.Next(2, 15);
+            Suit = random.Next(1, 5);
+
+        }
+
+        public readonly int Number;
+        public readonly int Suit;
+        public readonly int Name;
+    }
+}
