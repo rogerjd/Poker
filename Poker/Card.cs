232a2ba879c0ebcb5beec31ac6c0ee22410a432c
roger <rjpd@netzero.com>
2018-12-22 22:39:04 -0500

    Card

--- Poker/Card.cs
+++ Poker/Card.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Poker
 {
-    public class Card
+    internal class Card
     {
         public Card() : this(new Random()) { }
 
@@ -17,13 +17,45 @@ namespace Poker
             Name = numberArray[Number - 2] + suitArray[Suit - 1];
         }
 
+        public Card(string name)
+        {
+            string n = name.Substring(0, 1);
+            string s = name.Substring(1, 1);
+            Number = numberString.IndexOf(n) + 2;
+            Suit = suitString.IndexOf(s) + 1;
+            Name = name;
+        }
+
+        public override string ToString() 
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Card c)
+            {
+                return (c.Number == Number) && (c.Suit == Suit);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return (Suit << 4) + Number;
+        }
+
         public readonly int Number;
         public readonly int Suit;
         public readonly string Name;
 
-        private static string[] numberArray = { };
-        private static string[] suitArray = { };
-        private static string numberString = "";
-        private static string suitString = "";
+        private static string[] numberArray = {"2", "3", "4", "5", "6", "7", "8", "9", "T",
+            "J", "Q", "K", "A"};
+        private static string[] suitArray = {"C", "D", "H", "S" };
+        private static string numberString = "23456789TJQKA";
+        private static string suitString = "CDHS";
     }
 }
