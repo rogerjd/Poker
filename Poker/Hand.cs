2802355fc1d8ce17e3464c5530a6f404748a4ccc
roger <rjpd@netzero.com>
2018-12-23 04:57:15 -0500

    Hand, ctors, wip

---
+++ Poker/Hand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    using System;
+
+    public class Hand
+    {
+        public Hand()
+        {
+
+        }
+
+        public Hand(string handText)
+        {
+
+        }
+
+        public Hand(string handText, string holdString)
+        {
+
+        }
+
+        public Hand(Hand hand, string holdString)
+        {
+
+        }
+    }
+}
