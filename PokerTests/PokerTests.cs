ae37dfa50c2d671d2612c5965ebe2e6053cb483c
roger <rjpd@netzero.com>
2019-01-05 05:33:36 -0500

    Rensmr

--- PokerTests/PokerTests.cs
+++ PokerTests/PokerTests.cs
@@ -8,7 +8,7 @@ namespace PokerTests
     public class ScoreTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ThreeOfAKind()
         {
             Hand h = new Hand("3C 3S 3H 4D JD");  
             Assert.AreEqual(h.Score, 4);
